526b7d686df414b2194128f63e11054798c965b2
MateusGomes04 <mateusgomesc04@gmail.com>
2023-02-10 00:16:37 -0300

    feat: Nested Attributes with has_many

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 
   gem "faker"
+  gem 'pry-rails'
 end
 
 group :development do
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,17 +2,26 @@ class Contact < ApplicationRecord
   belongs_to :kind, optional: true 
   has_many :phones
 
+
+  accepts_nested_attributes_for :phones 
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
+  end
+
   # def birthdate_br
     #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
 
-  def to_br
-    {
-      name: self.name, 
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    }   
-  end
+  # def to_br
+  #   {
+  #     name: self.name, 
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }   
+  # end
     # def author
     #     "Mateus Gomes"
     # end
