b353657ac95fb132cfa798c1fcfd875370f9f891
MateusGomes04 <mateusgomesc04@gmail.com>
2023-02-13 09:10:58 -0300

    feat: Relationships and Routes (has_many)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -3,7 +3,9 @@ class ContactSerializer < ActiveModel::Serializer
   belongs_to :kind do 
     link(:related) { contact_kind_url(object.id) }
   end
-  has_many :phones
+  has_many :phones do
+    link(:related) { contact_kind_url(object.id) }
+  end
   has_one :address
 
   # link(:self) { contact_url(object.id) }
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :contacts do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
+    
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
   end
 
 
