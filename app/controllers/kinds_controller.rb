d74ab1b2dc460099d5c01e82cd238da996f6fec3
MateusGomes04 <mateusgomesc04@gmail.com>
2023-02-15 08:31:40 -0300

    feat: JWT (Rails)

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,10 +1,14 @@
 class KindsController < ApplicationController
 
+  # TOKEN = "secret123"
+
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
   # # http_basic_authenticate_with name: "jack", password: "secret"
 
-  include ActionController::HttpAuthentication::Digest::ControllerMethods
-  USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+    # include ActionController::HttpAuthentication::Digest::ControllerMethods
+  # USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :authenticate 
   before_action :set_kind, only: %i[ show update destroy ]
@@ -61,10 +65,20 @@ class KindsController < ApplicationController
     def kind_params
       params.require(:kind).permit(:description)
     end
-    
+
     def authenticate
-      authenticate_or_request_with_http_digest("Application") do |username|
-        USERS[username]
+       # authenticate_or_request_with_http_digest("Application") do |username|
+      #   USERS[username]
+      # end
+      authenticate_or_request_with_http_token do |token, options|
+        hmac_secret = 'my$ecretK3y'
+        JWT.decode token, hmac_secret, true, { :algorithm => 'HS256' }
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        # ActiveSupport::SecurityUtils.secure_compare(
+        #   ::Digest::SHA256.hexdigest(token),
+        #   ::Digest::SHA256.hexdigest(TOKEN)
+        )
       end
     end
 end
