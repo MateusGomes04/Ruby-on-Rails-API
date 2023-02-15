1a3c36f38039e9eec1c81f56edfbfc17e9d11b87
MateusGomes04 <mateusgomesc04@gmail.com>
2023-02-15 11:18:20 -0300

    feat: configuring the URL Segment

--- config/routes.rb
+++ config/routes.rb
@@ -3,27 +3,34 @@ Rails.application.routes.draw do
   # resources :auths, only: [:create]
   resources :kinds
 
-  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1.0"}) do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
+
       resource :phones, only: [:show]
       resource :phones, only: [:show], path: 'relationships/phones'
+
       resource :phone, only: [:update, :create, :destroy]
       resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
       resource :address, only: [:show, :update, :create, :destroy]
       resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
     end
   end
 
-  api_version(:module => "V2", :header => {:name => "X-Version", :value => "2.0"}) do
+  
+  api_version(:module => "V2", :path => {:value => "v2"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
+
       resource :phones, only: [:show]
       resource :phones, only: [:show], path: 'relationships/phones'
+
       resource :phone, only: [:update, :create, :destroy]
       resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
       resource :address, only: [:show, :update, :create, :destroy]
       resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
     end
