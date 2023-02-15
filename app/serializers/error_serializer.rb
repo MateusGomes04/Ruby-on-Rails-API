3d49c0a65f5c12325c990731d5fe3b77503e1cb4
MateusGomes04 <mateusgomesc04@gmail.com>
2023-02-15 12:13:06 -0300

    feat: handling errors according to the json api specification

---
+++ app/serializers/error_serializer.rb
@@ -0,0 +1,16 @@
+class ErrorSerializer < ActiveModel::Serializer
+  def self.serialize(errors)
+    return if errors.nil?
+
+    json = {}
+
+    new_hash = errors.to_hash.map do |k, v|
+      v.map do |msg|
+        { id: k, title: msg }
+      end
+    end.flatten
+
+    json[:errors] = new_hash
+    json
+  end
+end
